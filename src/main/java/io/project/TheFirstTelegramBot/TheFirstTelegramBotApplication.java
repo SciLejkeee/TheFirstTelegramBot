7d6bdf6bd70abb91152971a8ba47303d1195dc5f
SciLejkeee <parfenov.anatoly@mail.ru>
2022-09-10 15:19:01 +0500

    Created first steps in Telegramm Bot programming

---
+++ src/main/java/io/project/TheFirstTelegramBot/TheFirstTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.TheFirstTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFirstTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFirstTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/TheFirstTelegramBot/TheFirstTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.TheFirstTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFirstTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
